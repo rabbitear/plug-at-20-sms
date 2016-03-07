7b4f314f6f4c1e81bd49d65144e09dbc3a8f5025
Brendan Babb <brendanbabb@gmail.com>
2016-03-06 17:10:29 -0900

    Added correct index.js

--- index.js
+++ index.js
@@ -2,17 +2,15 @@
 
 // 3rd party library imports
 var express = require('express')
-var low = require('lowdb')
-var storage = require('lowdb/file-sync')
 var body_parser = require('body-parser')
 
 // project imports
 var text = require('./message_text.json')
 var cron_job = require('./cron_job')
+var db = require('./db')
 
 
 var app = express() // instantiate express
-var db = low('db.json', { storage }); // instantiate database
 
 
 // serve files from the public dir for testing via web
@@ -24,26 +22,10 @@ app.use(body_parser.urlencoded({ extended: true }))
 app.post('/', function(req, res, next) {
     var message = req.body.Body
     var phone_number = req.body.From
-    var zipcode_regex = /^\d{5}(?:[-\s]\d{4})?$/
-    var zip = null
 
-    var match = message.match(zipcode_regex)
-    if (match) zip = match[0]
-
-    // if they sent a zipcode
-    if (zip) {
-        db('subscribers').push({
-            phone: phone_number,
-            zip: zip,
-        })
-        return res.send(text.CONFIRMATION_MESSAGE)
-    }
-    // else just say Hello
-    else {
-        return res.send(text.WELCOME_MESSAGE)
-    }
+    // Comfirmation message
+    return res.send(text.CONFIRMATION_MESSAGE)
 });
-
 
 // start the server
 var port = process.env.PORT || 3000
