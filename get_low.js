5019dfe508cf5ad3e7e0fe7815b17ce6c58e71c8
Brendan Babb <brendanbabb@gmail.com>
2016-03-06 16:40:00 -0900

    Prints minimum temperature between 3am -9am

--- get_low.js
+++ get_low.js
@@ -3,7 +3,12 @@ var request = require('request');
 request('http://api.openweathermap.org/data/2.5/forecast?q=Anchorage,us&mode=xml&appid=44db6a862fba0b067b1930da0d769e98', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     parseString(body, function (err, result) {
-    console.dir(result.weatherdata.forecast[0].time[3]);
+    early_morning = parseFloat(result.weatherdata.forecast[0].time[1].temperature[0].$.min);
+    console.dir(early_morning);
+    morning = parseFloat(result.weatherdata.forecast[0].time[2].temperature[0].$.min);
+    console.dir(morning);
+    temp_min = Math.min(early_morning,morning);
+    console.dir(temp_min);
     });
   }
 })
