606ba7f4a60a08a4644c1b8c5654f5a50b7a2caa
Brendan Babb <brendanbabb@gmail.com>
2016-03-06 17:03:37 -0900

    Changed to function but not sure how to return the boolean

--- get_low.js
+++ get_low.js
@@ -1,5 +1,7 @@
 var parseString = require('xml2js').parseString;
 var request = require('request');
+
+function get_temp(){
 request('http://api.openweathermap.org/data/2.5/forecast?q=Anchorage,us&mode=xml&appid=44db6a862fba0b067b1930da0d769e98', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     parseString(body, function (err, result) {
@@ -9,6 +11,12 @@ request('http://api.openweathermap.org/data/2.5/forecast?q=Anchorage,us&mode=xml
     console.dir(morning);
     temp_min = Math.min(early_morning,morning);
     console.dir(temp_min);
+    if (temp_min < -6.66)
+       { return true; }
+    else
+       { return false; }
     });
   }
-})
+})}
+
+console.dir(get_temp());
